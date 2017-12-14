ba266c6836ece843eb986c9eb45eef9539fbe2de
AnandSundar <anandsundar96@gmail.com>
2017-12-14 12:30:55 -0500

    added components for LanguageSelect, TranslateInput and TranslateOutput

---
+++ app/components/TranslateInput/TranslateInput.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class TranslateInput extends Component<{}> {
+  render() {
+    return (
+      <View>
+        <Text>
+          Translate Input
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import LanguageSelect from './app/components/LanguageSelect/LanguageSelect';
+import TranslateOutput from './app/components/TranslateOutput/TranslateOutput';
+import TranslateInput from './app/components/TranslateInput/TranslateInput';
 
+AppRegistry.registerComponent('LanguageSelect', () => LanguageSelect);
+AppRegistry.registerComponent('TranslateOutput', () => TranslateOutput);
+AppRegistry.registerComponent('TranslateInput', () => TranslateInput);
 AppRegistry.registerComponent('weTranslate', () => App);
